13ac23e7c0164c7c4f1de64eda1ef323b3301832
unknown <mmohamedabdelm@gmail.com>
2023-08-01 11:02:35 +0300

    add frontend , add  in view.py if this answesr correct ot not  'mohamed  focus in view in this comment'

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -3,5 +3,5 @@ from .views import Question_List, Question_Detail
 
 urlpatterns = [
     path('q/', Question_List.as_view()),
-    path('q/<int:Q_id>', Question_Detail),
+    path('q/<int:Q_id>/', Question_Detail),
 ]
--- quiz_app/views.py
+++ quiz_app/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from .models import Question, Answer
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
 # Create your views here.
 
 class Question_List(ListView):
     model = Question
     context_object_name = "question"
 
+
+
+
 def Question_Detail(request, Q_id):
     data = Question.objects.get(id=Q_id)
     answers = Answer.objects.filter(question_title=data)
-    return render(request, 'quiz_app/qesiton_detail.html', {'Q':data, 'answers':answers})
+
+    if request.method == 'POST':
+        selected_answer_id = request.POST.get('answer')  # Get the ID of the selected answer from the form
+
+        try:
+            selected_answer = Answer.objects.get(id=selected_answer_id)
+            if selected_answer.correct_answer:
+                is_correct_answer = True
+            else:
+                is_correct_answer = False
+        except Answer.DoesNotExist:
+            is_correct_answer = False
+
+    else:
+        is_correct_answer = None
+
+    return render(request, 'quiz_app/question_detail.html', {'Q': data, 'answers': answers, 'is_correct_answer': is_correct_answer})
