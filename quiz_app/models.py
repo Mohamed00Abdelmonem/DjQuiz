c5dcb2d07933911d33a73a501b938913ceb2575d
unknown <mmohamedabdelm@gmail.com>
2023-08-01 09:30:11 +0300

    add model Question , add tagit , add

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
+from django.utils import timezone
 # Create your models here.
+
+class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    question_title = models.CharField(max_length=100)
+    content = models.TextField(max_length=1000)
+    slug = models.SlugField(unique=True, max_length=100)
+    tags = TaggableManager()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return self.question_title
