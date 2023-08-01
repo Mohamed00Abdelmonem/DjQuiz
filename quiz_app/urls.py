bd7896eed643a2797f0aa53f92b9e62f0974f7ab
unknown <mmohamedabdelm@gmail.com>
2023-08-01 17:24:49 +0300

    add breadcrumb , base bage

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import Question_List, Question_Detail
 
 urlpatterns = [
-    path('q/', Question_List.as_view()),
-    path('q/<int:Q_id>/', Question_Detail),
+    path('', Question_List.as_view()),
+    path('<int:Q_id>/', Question_Detail),
 ]
